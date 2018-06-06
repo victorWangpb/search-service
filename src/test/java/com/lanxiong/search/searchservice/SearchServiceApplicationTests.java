afa89df58a65e9203bd894a3ef372d93a689dfd8
wangpb <wangpb@lanxiong.com>
2018-06-06 21:21:56 +0800

    logger

--- src/test/java/com/lanxiong/search/searchservice/SearchServiceApplicationTests.java
+++ src/test/java/com/lanxiong/search/searchservice/SearchServiceApplicationTests.java
@@ -1,16 +1,27 @@
 package com.lanxiong.search.searchservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SearchServiceApplicationTests {
 
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void loggerTest(){
+		log.info("info...........");
+//		log.debug("debug...........");
+//		log.error("err...........");
+//		log.warn("warn...........");
 	}
 
 }
